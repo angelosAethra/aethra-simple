c81d3625c408a1e2e936506c4b9e7a8229413b7a
Angelos Lampiris <angelos@aethra.io>
2021-09-07 19:09:09 +0300

    Second attempt static files

--- django_app/settings.py
+++ django_app/settings.py
@@ -131,6 +131,4 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
